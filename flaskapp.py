e37190b323a13ecfac07ed074c322b2de4a02d3d
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2018-10-27 22:04:51 +0300

    Initial support for dynamic view

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 # from flask_cors import CORS
 from branch_mapper import get_tree
+from graph import get_graph_output
 
 app = Flask(__name__)
 # CORS(app)
@@ -15,5 +16,19 @@ def tree_by_year():
     year= request.args.get('year')
     if year < 1900:
         return jsonify({'success': True, 'tree': get_tree(year)}), 200
+    else:
+        return jsonify({'success': False}), 400
+
+@app.route('/dynamic', methods=['GET'])
+def dynamic():
+    return render_template('dynamic.html')
+
+@app.route('/graph_by_year', methods=['GET'])
+def graph_by_year():
+    year = request.args.get('year')
+    width = request.args.get('width')
+    height = requests.args.get('height')
+    if year < 1900:
+        return jsonify({'success': True, 'tree': get_graph_output(year,width,height)}), 200
     else:
         return jsonify({'success': False}), 400

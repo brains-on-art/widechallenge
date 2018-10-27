b83163092fb2ee1f57950a297b7b63594ca35cc0
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2018-10-27 14:08:17 +0300

    Better image finder

--- finna_image_finder.py
+++ finna_image_finder.py
@@ -2,26 +2,40 @@ import random
 from collections import namedtuple
 import joblib
 from finna_client import FinnaClient
+from math import ceil
+from tqdm import tqdm
 
 ImageRecord = namedtuple('ImageRecord', ['id', 'subject', 'url'])
 
-memory = joblib.Memory('./cache', verbose=1)
+memory = joblib.Memory('./cache', verbose=0)
+
 
 @memory.cache(ignore=['client'])
 def finna_search(year_a, year_b, client):
-    res = client.search('',
-                        fields=['id', 'subjects', 'images'],
-                        filters=['format:0/Image/', 
-                                 'online_boolean:1', 
-                                 'search_daterange_mv:"[{} TO {}]"'.format(year_a, year_b)], 
-                        other={'search_daterange_mv_type':'within'},
-                        limit=100)
-    records = res['records']
-    ids = [x['id'] for x in records]
-    subjects = [x['subjects'][0][0] if len(x['subjects'])>0 else [] for x in records]
-    images = [x['images'][0] if len(x['images'])>0 else [] for x in records]
-    base_url = 'https://finna.fi'
-    return [ImageRecord(id_, subj, base_url+img) for id_, subj, img in zip(ids, subjects, images) if subj != [] and img != []]
+    search = lambda page, n: client.search('',
+                                           fields=['id', 'subjects', 'images'],
+                                           filters=['format:0/Image/',
+                                                    'online_boolean:1',
+                                                    'search_daterange_mv:"[{} TO {}]"'.format(year_a, year_b)],
+                                           other={'search_daterange_mv_type':'within'},
+                                           page=page,
+                                           limit=n)
+    n_images = search(1,0)['resultCount']
+
+    res = []
+    for i in tqdm(range(ceil(n_images/100))):
+        records = search(i+1, 100)['records']
+
+        ids = [x['id'] for x in records]
+        subjects = [x['subjects'][0][0] if len(x['subjects']) > 0 else [] for x in records]
+        images = [x['images'][0] if len(x['images']) > 0 else [] for x in records]
+
+        base_url = 'https://finna.fi'
+
+        res += [ImageRecord(id_, subj, base_url+img) for id_, subj, img in zip(ids, subjects, images)
+                if subj != [] and img != []]
+
+    return res
     
 
 class FinnaImageFinder:
@@ -34,7 +48,8 @@ class FinnaImageFinder:
         res = finna_search(a, b, self.client)
         random.seed(42)
         return random.sample(res, k=n)
-        
+
+
 if __name__ == '__main__':
     finder = FinnaImageFinder()
     print(finder(1900))

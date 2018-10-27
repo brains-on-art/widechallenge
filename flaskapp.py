3d6a0dd2215c515c8e199855f4f061bcf9454734
Henri Kotkanen <henri.kotkanen@gmail.com>
2018-10-27 11:22:20 +0300

    move demo to templates to be rendered by flask

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, jsonify, current_app
-from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template
+# from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route('/', methods=['GET'])
+def index():
+    return render_template('demo.html')
+
+@app.route('/tree_by_year', methods=['GET'])
 def tree_by_year():
     """GET with a year as a query param"""
     year= request.args.get('year')

fc4a5a5b4798dfe4442b9c3f728716d75e34645f
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2018-10-27 16:36:24 +0300

    Tree creation code

--- branch_mapper.py
+++ branch_mapper.py
@@ -5,7 +5,7 @@ import joblib
 memory = joblib.Memory('./cache', verbose=0)
 
 
-@memory.cache
+@memory.cache(ignore=['sk'])
 def get_branch_route(keyword, sk):
 
     results = sk.search(keyword, vocabs='yso')
@@ -32,6 +32,67 @@ def get_branch_route(keyword, sk):
 
     return br_list
 
+
+#@memory.cache(ignore=['sk'])
+def get_node_parents(node, sk):
+    concept = sk.get_concept('yso', node['uri'])
+    parent_node = concept.broader()
+
+    if parent_node == []:
+        return [node]
+
+    return get_node_parents(parent_node[0], sk) + [node]
+
+
+def get_keyword_chain(keyword, sk):
+    results = sk.search(keyword + '*', vocabs='yso')
+
+    if results:
+        node = results[0]
+        node = {'uri': node['uri'], 'prefLabel': node['prefLabel'], }
+
+        chain = get_node_parents(node, sk)
+
+        return chain
+
+    else:
+        return None
+
+
+def create_tree(chain):
+    tree = {'prefLabel': 'root', 'count': 1, 'children': []}
+    root = tree
+    for node in chain:
+        n = node.copy()
+        n['count'] = 1
+        n['children'] = []
+        root['children'] += [n]
+        root = n
+    return tree
+
+
+def join_tree(tree_a, tree_b):
+    tree = tree_a.copy()
+    if tree['prefLabel'] == tree_b['prefLabel']:
+        tree['count'] += tree_b['count']
+
+        for c in tree_b['children']:
+            try:
+                ind = [x['prefLabel'] == c['prefLabel'] for x in tree['children']].index(True)
+                tree['children'][ind] = join_tree(tree['children'][ind], c)
+            except ValueError as e:
+                tree['children'].append(c)
+
+    return tree
+
+
+def join_trees(trees):
+    tree = trees[0].copy()
+    for t in trees[1:]:
+        tree = join_tree(tree, t)
+    return tree
+
+
 if __name__ == '__main__':
 
     sk = SkosmosClient()
@@ -41,7 +102,10 @@ if __name__ == '__main__':
     keywords = ['häst', 'teknik', 'porträtt']
 
     for keyword in keywords:
-        br = get_branch_route(keyword, sk)
+        br = get_keyword_chain(keyword, sk)
 
         for item in br:
             print('{}\n⬇️'.format(item))
+
+    tree = create_tree(get_keyword_chain('hatut', sk))
+    print(tree)

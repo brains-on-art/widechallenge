43c351bb95d73e78ad4f47a0647e9720273f10ff
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2018-10-27 18:33:35 +0300

    Flask app becomes interactive

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 # from flask_cors import CORS
+from branch_mapper import get_tree
 
 app = Flask(__name__)
 # CORS(app)
@@ -12,7 +13,7 @@ def index():
 def tree_by_year():
     """GET with a year as a query param"""
     year= request.args.get('year')
-    if year:
-        return jsonify({'success': True, 'tree': 'asdf'}), 200
+    if year < 1900:
+        return jsonify({'success': True, 'tree': get_tree(year)}), 200
     else:
         return jsonify({'success': False}), 400
